71ebe00e3b946be1722b2821df994a769e8d3361
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 16:34:24 +0100

    load the data set

--- foodsales_category_wise_analysis.py
+++ foodsales_category_wise_analysis.py
@@ -4,3 +4,7 @@ import seaborn as sns
 
 # Set seaborn style
 sns.set(style="whitegrid")
+
+# Load the Excel data
+file_path = "Sampledatafoodslaes.xlsx"
+df = pd.read_excel(file_path, sheet_name="FoodSales")
