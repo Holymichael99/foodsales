5642ed6c38c5cc2f1a54550ef477fc13ab7044c5
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 16:34:57 +0100

    preprocessing

--- foodsales_category_wise_analysis.py
+++ foodsales_category_wise_analysis.py
@@ -8,3 +8,6 @@ sns.set(style="whitegrid")
 # Load the Excel data
 file_path = "Sampledatafoodslaes.xlsx"
 df = pd.read_excel(file_path, sheet_name="FoodSales")
+
+# Preprocessing
+df['TotalSales'] = df['Quantity'] * df['UnitPrice']
