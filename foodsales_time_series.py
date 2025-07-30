57aeae3cf109485d47e1ea0a878ad755ddc9f107
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 16:26:35 +0100

    feature engineering

--- foodsales_time_series.py
+++ foodsales_time_series.py
@@ -11,3 +11,9 @@ df = pd.read_excel(file_path, sheet_name="FoodSales")
 
 # Convert OrderDate to datetime
 df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+
+# Feature engineering
+df['TotalSales'] = df['Quantity'] * df['UnitPrice']
+df['Year'] = df['OrderDate'].dt.year
+df['Month'] = df['OrderDate'].dt.to_period('M').astype(str)
+df['Week'] = df['OrderDate'].dt.to_period('W').astype(str)
