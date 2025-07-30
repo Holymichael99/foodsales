1ccd7bdb9f7e485ff10001d5a091054aa7913e0d
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 15:46:22 +0100

    load the excel file

--- foodsales_eda.py
+++ foodsales_eda.py
@@ -6,3 +6,7 @@ import numpy as np
 
 # Optional: set plot style
 sns.set(style="whitegrid")
+
+# Load the Excel file
+file_path = "Sampledatafoodslaes.xlsx"
+data = pd.read_excel(file_path, sheet_name="FoodSales")
