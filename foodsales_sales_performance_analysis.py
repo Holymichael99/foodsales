d4f3a295dcfb3102f7f136904b0fb99c5ccf73fd
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 15:59:52 +0100

    convert order date to date time format

--- foodsales_sales_performance_analysis.py
+++ foodsales_sales_performance_analysis.py
@@ -5,3 +5,6 @@ import pandas as pd
 # Load Excel file and select the 'FoodSales' sheet
 file_path = "Sampledatafoodslaes.xlsx"  # Ensure this file is in the same directory
 df = pd.read_excel(file_path, sheet_name="FoodSales")
+
+# Convert OrderDate to datetime format
+df['OrderDate'] = pd.to_datetime(df['OrderDate'])
