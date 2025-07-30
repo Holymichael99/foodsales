bc4adac52ad10c7ab13ff43e5cf24ecdfa9e8737
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 16:27:17 +0100

    monthly sales trend

--- foodsales_time_series.py
+++ foodsales_time_series.py
@@ -17,3 +17,21 @@ df['TotalSales'] = df['Quantity'] * df['UnitPrice']
 df['Year'] = df['OrderDate'].dt.year
 df['Month'] = df['OrderDate'].dt.to_period('M').astype(str)
 df['Week'] = df['OrderDate'].dt.to_period('W').astype(str)
+
+# --------------------------------------------
+# 1. Monthly Sales Trend
+# --------------------------------------------
+monthly_sales = df.groupby('Month')['TotalSales'].sum().reset_index()
+
+# Sort by date
+monthly_sales['Month'] = pd.to_datetime(monthly_sales['Month'])
+monthly_sales = monthly_sales.sort_values('Month')
+
+plt.figure(figsize=(10, 4))
+sns.lineplot(data=monthly_sales, x='Month', y='TotalSales', marker='o')
+plt.title("Monthly Sales Trend")
+plt.xlabel("Month")
+plt.ylabel("Total Sales")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
