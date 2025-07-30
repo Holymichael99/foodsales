a7e265047c06b34898bccc0269f62160231ffc7a
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 16:18:48 +0100

    Convert OrderDate to datetime and create TotalSales

--- foodsales_top_bottom_performers.py
+++ foodsales_top_bottom_performers.py
@@ -8,3 +8,7 @@ sns.set(style="whitegrid")
 # Load Excel data
 file_path = "Sampledatafoodslaes.xlsx"  # Make sure this file is in the same folder
 df = pd.read_excel(file_path, sheet_name="FoodSales")
+
+# Convert OrderDate to datetime and create TotalSales
+df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+df['TotalSales'] = df['Quantity'] * df['UnitPrice']
