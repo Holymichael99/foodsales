86e9bc61cda80ec250331b12f6c0a28337133dae
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 16:18:07 +0100

    load dataset

--- foodsales_top_bottom_performers.py
+++ foodsales_top_bottom_performers.py
@@ -4,3 +4,7 @@ import seaborn as sns
 
 # Set seaborn style
 sns.set(style="whitegrid")
+
+# Load Excel data
+file_path = "Sampledatafoodslaes.xlsx"  # Make sure this file is in the same folder
+df = pd.read_excel(file_path, sheet_name="FoodSales")
