25b0aa9b903d534def62fc4f53df92cd5b4fa2aa
Holymichael99 <omofolarin1994@gmail.com>
2025-07-30 15:58:56 +0100

    load dataset

--- foodsales_sales_performance_analysis.py
+++ foodsales_sales_performance_analysis.py
@@ -1,3 +1,7 @@
 # food_sales_analysis.py
 
 import pandas as pd
+
+# Load Excel file and select the 'FoodSales' sheet
+file_path = "Sampledatafoodslaes.xlsx"  # Ensure this file is in the same directory
+df = pd.read_excel(file_path, sheet_name="FoodSales")
