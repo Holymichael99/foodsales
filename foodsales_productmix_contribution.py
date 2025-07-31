9f7db7bcc319132021b32080f1af474b812b23b2
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 09:04:57 +0100

    category contribution

--- foodsales_productmix_contribution.py
+++ foodsales_productmix_contribution.py
@@ -6,3 +6,6 @@ import seaborn as sns
 file_path = "Sampledatafoodslaes.xlsx"
 df = pd.read_excel(file_path, sheet_name="FoodSales")
 df["Sales"] = df["UnitPrice"] * df["Quantity"]
+
+# Category Contribution
+category_sales = df.groupby("Category")["Sales"].sum().sort_values(ascending=False).reset_index()
