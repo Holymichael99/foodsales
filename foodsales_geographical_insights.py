d032915664b8ce01f9af89562564cf74c2411986
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 08:50:54 +0100

    Sales by Region

--- foodsales_geographical_insights.py
+++ foodsales_geographical_insights.py
@@ -7,4 +7,10 @@ file_path = "Sampledatafoodslaes.xlsx"
 df = pd.read_excel(file_path, sheet_name="FoodSales")
 
 # Create 'Sales' column
-df["Sales"] = df["UnitPrice"] * df["Quantity"]
+
+# Sales by Region
+plt.subplot(1, 2, 1)
+sns.barplot(data=region_summary, x="Region", y="Sales", palette="Blues_d")
+plt.title("Total Sales by Region")
+plt.ylabel("Sales")
+plt.grid(True)
