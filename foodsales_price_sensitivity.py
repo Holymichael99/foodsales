8ebb45aab8e3469c7711b6d056516d3bed09f3c0
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 08:36:39 +0100

    Update foodsales_price_sensitivity.py

--- foodsales_price_sensitivity.py
+++ foodsales_price_sensitivity.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Load the data
+file_path = "Sampledatafoodslaes.xlsx"
+df = pd.read_excel(file_path, sheet_name="FoodSales")
