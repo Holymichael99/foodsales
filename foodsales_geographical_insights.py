5deb4d2245283118221f72209d1824ed64885614
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 08:48:00 +0100

    load the dataset

--- foodsales_geographical_insights.py
+++ foodsales_geographical_insights.py
@@ -2,3 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load data
+file_path = "Sampledatafoodslaes.xlsx"
+df = pd.read_excel(file_path, sheet_name="FoodSales")
