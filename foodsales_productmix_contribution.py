a4eae1187003422a9c6707be0d182c1babf69356
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 09:04:05 +0100

    load and prepare the data

--- foodsales_productmix_contribution.py
+++ foodsales_productmix_contribution.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Load and prepare data
+file_path = "Sampledatafoodslaes.xlsx"
+df = pd.read_excel(file_path, sheet_name="FoodSales")
+df["Sales"] = df["UnitPrice"] * df["Quantity"]
