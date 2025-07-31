734412c6b15f00488b0696a65d56f77c961a5f9f
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 08:49:23 +0100

    create sales column

--- foodsales_geographical_insights.py
+++ foodsales_geographical_insights.py
@@ -5,3 +5,6 @@ import seaborn as sns
 # Load data
 file_path = "Sampledatafoodslaes.xlsx"
 df = pd.read_excel(file_path, sheet_name="FoodSales")
+
+# Create 'Sales' column
+df["Sales"] = df["UnitPrice"] * df["Quantity"]
